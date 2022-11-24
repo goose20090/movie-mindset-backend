55ef62a0ec28767e9cc69e5d21276875af5e113c
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-11-24 15:22:00 +0000

    re-seeded data in games and reviews table, one-to-many connection verified in console

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,20 @@ streaming_services = ["Disney+", "Netflix", "Amazon Prime"]
 genres = ["Action", "Horror", "Drama", "Thriller", "Western", "Sci-Fi", "Comedy", "Crime", "Romance", "Adventure"]
 50.times do
 
-    Movie.create(
+    movie = Movie.create(
         title: Faker::Movie.title,
-        genre: genres[rand(10)-1],
-        streaming_platform: streaming_services[rand(3)-1],
+        genre: genres[rand(10)],
+        streaming_platform: streaming_services[rand(3)],
         release_date: Faker::Date.between(from: 80.years.ago, to: Date.today)
     )
+
+    rand(1..5).times do
+        Review.create(
+            rating: rand(1..5),
+            comment: Faker::Lorem.sentence,
+            movie_id: movie.id
+        )
+    end
 end
 
 puts "✅ Done seeding!"
