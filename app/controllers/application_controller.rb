3504d1b86760ff74bf116502b711b75b01a98383
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-11-30 15:55:03 +0000

    get request for a specific movie's reviews now also returns those reviews' users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,21 @@ class ApplicationController < Sinatra::Base
     movies.to_json
   end
 
-  get "/reviews" do 
-    reviews = Review.all
-    reviews.to_json
+  # Returns a movie's review and relevant user
+  
+  get "/movies/:id/reviews" do 
+    movie = Movie.find(params[:id])
+    reviews = movie.reviews
+    users = reviews.map{|review| review.user}
+    reviews_with_user = reviews.map do |review|
+      index = reviews.index(review)
+      user = users[index]
+      return_hash = {
+        review: review,
+        user: user
+      }
+    end
+    reviews_with_user.to_json
   end
 
 end
