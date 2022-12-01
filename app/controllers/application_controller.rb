15be1819adb8dd296bc1ab7c44f9b288782f7107
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-12-01 11:30:32 +0000

    turned elaborate sinatra responses into proper OO instance methods, cleaning up conroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,27 +11,8 @@ class ApplicationController < Sinatra::Base
 
   get "/movies/:id/reviews" do 
     movie = Movie.find(params[:id])
-    reviews = movie.reviews
-    users = movie.users
-
-    # Work out the Average rating of all reviews
-    average_rating = reviews.sum(:rating) / reviews.count.to_f
-
-    # Make an array of hashes of each movie's review with its user
-    reviews_with_user = reviews.map do |review|
-      index = reviews.index(review)
-      user = users[index]
-      return_hash = {
-        review: review,
-        user: user
-      }
-    end
-
-    # Add the average rating
-
-    reviews_with_user_and_average_rating = { return_hash: reviews_with_user, average_rating: average_rating}
-
-    reviews_with_user_and_average_rating.to_json
+    return_hash = movie.get_reviews_with_user_and_average_rating
+    return_hash.to_json
   end
 
 end
--- app/models/Movie.rb
+++ app/models/Movie.rb
@@ -1,4 +1,20 @@
 class Movie < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+
+    def get_average_rating 
+        self.reviews.sum(:rating) / self.reviews.count.to_f
+    end
+
+    def get_reviews_with_user
+        self.reviews.map do |review|
+            index = self.reviews.index(review)
+            user = self.users[index]
+            {review: review, user: user}
+        end
+    end
+
+    def get_reviews_with_user_and_average_rating
+        {reviews_and_users: self.get_reviews_with_user, average_rating: self.get_average_rating}
+    end
 end
