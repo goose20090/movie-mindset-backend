748397a74563ddb33f74a86449568e7116dd02ec
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-12-01 15:24:18 +0000

    added a users path to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,23 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+  # Returns a JSON response of all instances of the Movie class
   get "/movies" do
     movies = Movie.all
     movies.to_json
   end
 
-  # Returns a movie's reviews and relevant users
+  # Returns a movie's reviews and relevant users, with that movie's average rating
 
   get "/movies/:id/reviews" do 
     movie = Movie.find(params[:id])
     return_hash = movie.get_reviews_with_user_and_average_rating
     return_hash.to_json
+  end
+
+  get "/users" do
+    users = User.all
+    users.to_json
   end
 
 end
--- app/models/Movie.rb
+++ app/models/Movie.rb
@@ -2,10 +2,12 @@ class Movie < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
 
+    # Gets a movie's average rating across all it's reviews
     def get_average_rating 
         self.reviews.sum(:rating) / self.reviews.count.to_f
     end
 
+    # Creates a hash of each review, paired with its parent user
     def get_reviews_with_user
         self.reviews.map do |review|
             index = self.reviews.index(review)
@@ -14,6 +16,8 @@ class Movie < ActiveRecord::Base
         end
     end
 
+    # Creates a hash of each review, pared with its user and the average rating of all reviews
+    
     def get_reviews_with_user_and_average_rating
         {reviews_and_users: self.get_reviews_with_user, average_rating: self.get_average_rating}
     end
