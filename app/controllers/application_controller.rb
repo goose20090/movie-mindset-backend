90d65f5724a1a62d4283e3b71f550cd479419e86
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-12-15 15:33:02 +0000

    removed unused routes from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,21 +9,9 @@ class ApplicationController < Sinatra::Base
 
   # Returns a movie's reviews and relevant users, with that movie's average rating
 
-  get "/movies/:id/reviews" do 
-    movie = Movie.find(params[:id])
-    return_hash = movie.get_reviews_with_user_and_average_rating
-    return_hash.to_json
-  end
-
   get "/users" do
     users = User.all
     users.to_json(include:{reviews: {include: :movie}})
-  end
-
-  get "/users/:id/reviews" do 
-    user = User.find(params[:id])
-    return_hash = user.get_reviews_with_movie
-    return_hash.to_json
   end
 
 end
