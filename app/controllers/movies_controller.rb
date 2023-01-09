d8d89efa00fff64beaa34cbbdf514aa33d4fff67
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-01-09 15:45:31 +0000

    added handler for movie post request

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,21 @@
 class MoviesController < ApplicationController
 
+    streaming_services = ["Disney+", "Netflix", "Amazon Prime"]
+genres = ["Action", "Horror", "Drama", "Thriller", "Western", "Sci-Fi", "Comedy", "Crime", "Romance", "Adventure"]
+
     # Returns all movies
     get "/movies" do
         movies = Movie.all
         movies.to_json(include:{reviews: {include: :user}})
+    end
+
+    post "/movies" do
+        movie = Movie.create(
+        title: params[:title],
+        genre: genres[rand(10)],
+        streaming_platform: streaming_services[rand(3)],
+        release_date: Faker::Date.between(from: 80.years.ago, to: Date.today)
+        )
+        movie.to_json(include:{reviews: {include: :user}})
     end
 end
