2734f47ad0b2d4efbe5bd92eec91b3abf66725d2
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-01-09 14:06:05 +0000

    removed unnecessary methods from class Models

--- app/models/Movie.rb
+++ app/models/Movie.rb
@@ -2,23 +2,4 @@ class Movie < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
 
-    # Gets a movie's average rating across all it's reviews
-    def get_average_rating 
-        self.reviews.sum(:rating) / self.reviews.count.to_f
-    end
-
-    # Creates a hash of each review, paired with its parent user and returns all in an array
-    def get_reviews_with_user
-        self.reviews.map do |review|
-            index = self.reviews.index(review)
-            user = self.users[index]
-            {review: review, user: user}
-        end
-    end
-
-    # Creates a hash of an array each review, paired with its user and the average rating of all reviews
-
-    def get_reviews_with_user_and_average_rating
-        {reviews_and_users: self.get_reviews_with_user, average_rating: self.get_average_rating}
-    end
 end
